3496bf343c2af575845d41b1932eba1a6e41390c
Tyler Sharp <johntylersharp@gmail.com>
2020-03-16 15:42:22 -0400

    added yearOfBirth function

--- drills.js
+++ drills.js
@@ -1,8 +1,11 @@
 /* eslint-disable strict */
 function createGreeting(name, age) {
     const yearOfBirth = 2020 - age;
-  
     return `Hello my name is ${name} and I'm ${age} and I was born in ${yearOfBirth}.`;
+}
+
+function yearOfBirth(age) {
+    return 2020 - age;
 }
 
 let greeting = createGreeting();

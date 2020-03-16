d41db8d9629b2d04431d531bf41e4ff75a52129a
Tyler Sharp <johntylersharp@gmail.com>
2020-03-16 15:56:44 -0400

    Added if statement to createGreeting

--- drills.js
+++ drills.js
@@ -1,5 +1,8 @@
 /* eslint-disable strict */
 function createGreeting(name, age) {
+    if (!name || !age) {
+        throw new Error("Arguments not valid")
+    }
   const yearOfBirth = 2020 - age;
   return `Hello my name is ${name} and I'm ${age} and I was born in ${yearOfBirth}.`;
 }
@@ -17,4 +20,5 @@ try {
   
 }
 
+console.log(createGreeting("Tyler", 28))
 console.log(greeting);

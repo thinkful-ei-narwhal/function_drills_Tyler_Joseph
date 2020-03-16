b524f2543ad765cff0c828e8dcfa52fdb2a13bff
Joseph Rivera <josephr98@gmail.com>
2020-03-16 15:51:55 -0400

    added error handling

--- drills.js
+++ drills.js
@@ -1,13 +1,20 @@
 /* eslint-disable strict */
 function createGreeting(name, age) {
-    const yearOfBirth = 2020 - age;
-    return `Hello my name is ${name} and I'm ${age} and I was born in ${yearOfBirth}.`;
+  const yearOfBirth = 2020 - age;
+  return `Hello my name is ${name} and I'm ${age} and I was born in ${yearOfBirth}.`;
 }
 
 function yearOfBirth(age) {
-    return 2020 - age;
+  if (age < 0) {
+    throw new Error('Age cannot be negative!');
+  }
+  return 2020 - age;
 }
 
-let greeting = createGreeting();
+try {
+  let greeting = createGreeting();
+} catch(err) {
+  
+}
 
 console.log(greeting);

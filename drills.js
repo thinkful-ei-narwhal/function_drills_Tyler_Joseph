c63ca072f0792230f0d66ad724151f053b9d48b8
Tyler Sharp <johntylersharp@gmail.com>
2020-03-16 15:28:19 -0400

    Created function createGreeting and called it

--- drills.js
+++ drills.js
@@ -1,1 +1,7 @@
-console.log("Hello my name is Joseph and I'm 21");
+/* eslint-disable strict */
+function createGreeting() {
+    console.log("Hello my name is Joseph and I'm 21");
+}
+
+createGreeting();
+

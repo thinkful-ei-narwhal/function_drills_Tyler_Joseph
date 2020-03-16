176d0281f6057929da91db1a20e22679ca23514d
Joseph Rivera <josephr98@gmail.com>
2020-03-16 16:23:17 -0400

    added function beyond

--- drills2.js
+++ drills2.js
@@ -6,3 +6,17 @@ function jediName(firstName, lastName) {
 };
 
 console.log(jediName("Joseph", "Tyler"))
+
+function beyond(num) {
+    if (num == Infinity) {
+        console.log('And beyond!');
+    } else if (num > 0) {
+        console.log('To infinity');
+    } else if (num < 0) {
+        console.log('To negative infinity');
+    } else {
+        console.log('Staying home');
+    }
+}
+
+beyond(Infinity);
